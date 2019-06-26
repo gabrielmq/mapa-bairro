4d6bf91e87278d308266056dd1ca957342fd6c3a
Gabriel <gabrielmqs04@gmail.com>
2019-06-26 00:22:44 -0300

    atualizando componente Alerta

--- src/components/Alerta.js
+++ src/components/Alerta.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
 import Snackbar from '@material-ui/core/Snackbar';
-import CloseIcon from '@material-ui/icons/Close';
 
-const styles = theme => ({
-  close: {
-    padding: theme.spacing(0.5)
-  }
-});
-class Alerta extends Component {
+export default class Alerta extends Component {
   render() {
-    const { exibir, carregando, mensagem, classes } = this.props;
+    const { exibir, mensagem } = this.props;
 
     return (
       <>
@@ -21,7 +13,7 @@ class Alerta extends Component {
             vertical: 'bottom',
             horizontal: 'left'
           }}
-          open={exibir || carregando}
+          open={exibir}
           autoHideDuration={6000}
           onClose={this.props.close}
           onExited={this.props.exited}
@@ -29,19 +21,8 @@ class Alerta extends Component {
             'aria-describedby': 'message-id'
           }}
           message={<span id="message-id">{mensagem}</span>}
-          action={[
-            <IconButton
-              key="close"
-              aria-label="Close"
-              color="inherit"
-              className={classes.close}
-            >
-              <CloseIcon />
-            </IconButton>
-          ]}
         />
       </>
     );
   }
 }
-export default withStyles(styles)(Alerta);
